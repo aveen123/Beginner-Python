1851047139cb7028ee90b16b7f35bf425c413f4b
Avishek Khatiwada <avishekkhatiwada35@gmail.com>
2024-10-23 23:49:21 -0400

    Learning about Boolean

---
+++ Boolean_and_If/main.py
@@ -0,0 +1,15 @@
+# Boolean are values which are True or False
+
+light_is_on = True  
+
+if light_is_on:
+    print("The light is on!")
+else:
+    print("The light is off!")
+
+light_is_on = False  
+
+if light_is_on:
+    print("The light is on!")
+else:
+    print("The light is off!")
---
+++ Boolean_and_If/try.py
@@ -0,0 +1,9 @@
+# Find something around you that could be represented by a Boolean and make a variable for it
+
+fan_turned_on = False
+
+if fan_turned_on:
+    print("The fan is turned off!")
+
+else:
+    print("The fan is turned on!")
