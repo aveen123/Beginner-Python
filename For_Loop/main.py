ca05d4444001765faf709501b7c0364613a6c0b4
Avishek Khatiwada <avishekkhatiwada35@gmail.com>
2024-10-24 00:52:39 -0400

    For Loop

---
+++ For_Loop/main.py
@@ -0,0 +1,8 @@
+# for number in range(10):
+#     print("Hello")
+#     print(number)
+
+fav_movies = ["Sandlot", "The Lego Movie", "Dune"]
+
+for movie in fav_movies:
+    print(movie)
---
+++ For_Loop/try.py
@@ -0,0 +1,4 @@
+# Loop 40 times and print the number of the loop times 2. Ex- 2, 4, 6, 8...
+
+for x in range(40):
+    print( (x + 1) * 2)
