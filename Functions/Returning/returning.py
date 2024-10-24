01eb94417147af0701163edbb24e8c660da5a1b4
Avishek Khatiwada <avishekkhatiwada35@gmail.com>
2024-10-24 01:44:14 -0400

    Returning a value from a function

---
+++ Functions/Returning/returning.py
@@ -0,0 +1,5 @@
+def double(number):
+    doubled_numnber = number * 2
+    return doubled_numnber
+
+print(double(5))
---
+++ Functions/Returning/try.py
@@ -0,0 +1,6 @@
+# Create a function that returns a string in all caps
+
+def all_caps(text):
+    return text.upper()
+
+print(all_caps("hello, my name is avishek."))
