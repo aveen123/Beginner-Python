346eafde113427bfb55b180412f3107fb51e5b80
Avishek Khatiwada <avishekkhatiwada35@gmail.com>
2024-10-23 23:26:52 -0400

    Learning about integers and floating point numbers

---
+++ ints_and_floats/main.py
@@ -0,0 +1,11 @@
+day = 21
+temp = -15
+
+# Floats 
+
+weight = 190.4
+
+print( 3 + 6 ) # addition
+print( day - 3 ) # Subtraction
+print( weight * 2 ) # multiplication
+print( day / 3 ) # division
---
+++ ints_and_floats/try.py
@@ -0,0 +1,6 @@
+# Find something around you in your life to represent an int and a float. Put them in variables
+
+age = 32
+bitcoin = 0.00435
+
+print(age, bitcoin) # two or more variables or output can be printed in the same line
