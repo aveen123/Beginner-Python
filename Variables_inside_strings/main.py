dc73bf3d3b7943bfe001ef87fb3d9dd61689853d
Avishek Khatiwada <avishekkhatiwada35@gmail.com>
2024-10-23 23:42:39 -0400

    Adding variables inside strings

---
+++ Variables_inside_strings/main.py
@@ -0,0 +1,6 @@
+day = 21
+month = "October"
+temp = 65
+
+# to put variables inside of the string using {}, we use "f" --> formatted string
+print(f"Today is {month} {day} and it's {temp} degrees outside.")
---
+++ Variables_inside_strings/try.py
@@ -0,0 +1,5 @@
+# Make a variable called day_name and set it to the day of the week like Tuesday and add it to the string we print
+
+day_name = "Wednesday"
+
+print(f"Today is {day_name}")
