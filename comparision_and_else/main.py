a602edf6166d80c7b650024269a5f135cc827239
Avishek Khatiwada <avishekkhatiwada35@gmail.com>
2024-10-23 23:55:18 -0400

    Learning about comparison

---
+++ comparision_and_else/main.py
@@ -0,0 +1,13 @@
+
+light_is_on = False
+
+if light_is_on:
+    print("The light is on!")
+else:
+    print("We're in the dark!")
+
+
+weight = 190.4
+
+if weight < 195:
+    print("You're underweight :)")
---
+++ comparision_and_else/try.py
@@ -0,0 +1,8 @@
+# Make a variable age, and if someone is 18 years or older, print that they are an adult. Otherwise print that they are a child
+
+age = 16
+
+if age >= 18:
+    print("Congratulations! You're an adult.")
+else:
+    print("Hello child!")
